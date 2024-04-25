1254a998a9f79dd57060100ad57ba81421649303
Dilshad <hmydilshadkt@gmail.com>
2024-04-25 22:37:14 +0530

    change:baseuirl

--- src/config/axois/Axios.js
+++ src/config/axois/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dignizant-api.onrender.com/api  ",
+  baseURL: "https://dignizant-api.onrender.com/api/",
   // baseURL: "http://localhost:8080/api/",
 });
 
